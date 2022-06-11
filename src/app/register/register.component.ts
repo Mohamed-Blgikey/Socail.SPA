d11a2bdaeee361790d3ac63a25faf4fe46635741
Mohamed Blgikey <a.z29620014@gmail.com>
2022-06-11 17:50:04 +0200

    EditRegisterForm

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,40 +1,75 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import {
+  AbstractControl,
+  FormControl,
+  FormGroup,
+  ValidationErrors,
+  ValidatorFn,
+  Validators,
+} from '@angular/forms';
 import { AuthService } from '../_Services/auth.service';
 
+export function passwordsMatchValidator(): ValidatorFn {
+  return (control: AbstractControl): ValidationErrors | null => {
+    const password = control.get('password')?.value;
+    const confirmPassword = control.get('confirmPassword')?.value;
+
+    if (password && confirmPassword && password !== confirmPassword) {
+      return { passwordsDontMatch: true };
+    } else {
+      return null;
+    }
+  };
+}
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent implements OnInit {
-
   @Output() cancelRegister = new EventEmitter();
 
-  registerForm :FormGroup = new FormGroup({
-    email:new FormControl(null,[Validators.email,Validators.required]),
-    password:new FormControl(null,[Validators.required]),
-    firstName:new FormControl(null,[Validators.required]),
-    lastName:new FormControl(null,[Validators.required]),
-  })
-  constructor(private auth:AuthService) { }
+  registerForm: FormGroup = new FormGroup(
+    {
+      email: new FormControl(null, [Validators.email, Validators.required]),
+      password: new FormControl(null, [Validators.required]),
+      confirmPassword: new FormControl(null, [Validators.required]),
+      firstName: new FormControl(null, [Validators.required]),
+      lastName: new FormControl(null, [Validators.required]),
+      country: new FormControl(null, [Validators.required]),
+      city: new FormControl(null, [Validators.required]),
+      dateOfBirth: new FormControl(null, [Validators.required]),
+      gender: new FormControl(null, [
+        Validators.required,
+        Validators.min(0),
+        Validators.max(1),
+      ]),
+    },
+    { validators: passwordsMatchValidator() }
+  );
+  constructor(private auth: AuthService) {}
 
-  ngOnInit() {
-  }
-  register(form:FormGroup) {
+  ngOnInit() {}
+  register(form: FormGroup) {
+
     // console.log(form.value);
-    this.auth.register(form.value).subscribe(res=>{
-      if(res.message == 'Success'){
+    if (form.controls['gender'].value === "0") {
+      form.controls['gender'].setValue(0);
+    }else{
+      form.controls['gender'].setValue(1);
+    }
+
+    this.auth.register(form.value).subscribe((res) => {
+      if (res.message == 'Success') {
         form.reset();
         console.log('تم الإشتراك');
-      }else{
+      } else {
         console.log(res.message);
       }
-    })
+    });
   }
   cancel() {
     // console.log('ليس الأن')
     this.cancelRegister.emit(false);
   }
-
 }
